235433c369991603eb4466ec8fd722db9e26b41a
VICTORIA AMORIM NOVAES DE SOUSA <victoria.ansousa@senacsp.edu.br>
2022-06-06 11:37:57 -0300

    Revisão de funçõess

---
+++ funcoes1.js
@@ -0,0 +1,44 @@
+/*
+function notificacao(){
+    alert("Salve meu")
+}
+
+notificacao()
+
+
+Função Anonima
+
+var teste = function (){
+    alert("Olá isso é um teste")
+    
+}
+teste()
+
+
+Função sem retorno
+
+function soma(a, b){ //1)   
+    console.log(a + b) 
+     //2)}soma(2,3) //3)
+
+}
+
+*/
+
+// Conversão de sentimetros para metros
+
+/*
+
+let a = prompt ("Digite aqui seu primeiro numero")
+let b = prompt ("Digite seu segundo numero")
+function conta(){
+    if (a >= b){
+        alert ("Esse numero é o maior!")
+    } else {
+        alert ("Esse numero é menor!")
+    }
+}
+conta()
+
+*/
+
---
+++ funcoes2.js
@@ -0,0 +1,54 @@
+
+/*
+
+let fah = prompt ("Digite aqui a temperatura em Fahrenheit")
+
+function conversao(celcius){
+    return (fah - 32)*5/9
+}
+conversao()
+
+alert ( +celcius)
+
+*/
+
+
+/*
+
+let velocity = 0
+let nave = prompt ("Digite aqui o nome da nave: ")
+let menu = true
+
+while (menu){
+    let opcoes = prompt ("1 - Acelere a nave em 5KM \n 2 - desacelerar a nave em 5KM \n 3 - Imprimir resultados \n 4 - Sair")
+    if(opcoes == 1){
+        velocityPlus()
+    } else if (opcoes == 2){
+        velocityDowngrade()
+    }else if (opcoes == 3){
+        dados()
+    }else if (opcoes == 4){
+        alert ("Saindo do pograma")
+    }else {
+        alert ("Opção invalida")
+    }
+}
+
+function velocityPlus(){
+    newVelocity = velocity += 5
+    return newVelocity
+}
+function velocityDowngrade(){
+    if (velocity <= 0){
+        alert ("A nave já está parada")
+    } else
+    newVelocity = velocity-=5
+}
+
+function dados(){
+    alert ("O nome da nave é " +nave+ " E a velocidade da nave é " +newVelocity)
+}
+
+*/
+
+
